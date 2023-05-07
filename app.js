e4c4127a2188a217557d3336012f395993ff0f2a
aysun gunduz <113923715+aysungunduz@users.noreply.github.com>
2023-05-07 12:33:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const monthNameEl = document.querySelector("#month-name");
+const dayNameEl = document.querySelector("#day-name");
+const dayNumEl = document.querySelector("#day-number");
+const yearEl = document.querySelector("#year");
+
+const date = new Date();
+const month = date.getMonth();
+
+monthNameEl.innerText = date.toLocaleString("en", {
+    month: "long"
+})
+
+dayNameEl.innerText = date.toLocaleString("en", {
+    weekday: "long"
+})
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
